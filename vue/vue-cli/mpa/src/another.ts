e09c21ed3fb8f3fc691a90d269749f90a75099de
gandb <gandbranco@gmail.com>
2019-02-10 10:35:54 -0200

    exemplo de aplicação multi page com vue cli usando router no cliente

---
+++ vue/vue-cli/mpa/src/another.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Another from './Another.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(Another)
+}).$mount('#app')
