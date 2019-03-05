27e713f2532ac7d13a6e47d4827f0c2b16b62086
Edson Carli <gandbranco@gmail.com>
2019-03-05 20:31:26 -0300

    semantic vue  - vue cli

---
+++ vue/vue-cli/semantic_vue_eg/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import SuiVue from 'semantic-ui-vue'
+
+
+Vue.config.productionTip = false
+
+
+Vue.use(SuiVue);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
