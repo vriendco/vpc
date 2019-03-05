05dca3b38701692b625e400a4731b4c894170f62
Edson Carli <gandbranco@gmail.com>
2019-03-05 20:28:45 -0300

    require

---
+++ require/index.js
@@ -0,0 +1,13 @@
+//doc in : https://expressjs.com/pt-br
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
