7a1557cce27ed779236d1679f3d9e6d6416290e9
Edson Carli <gandbranco@gmail.com>
2019-03-05 20:24:50 -0300

    Typescript webpack

---
+++ webpack/typescript-master/src/index.ts
@@ -0,0 +1,4 @@
+let s:string = "isso é um teste 2";
+
+
+document.write(s);
---
+++ webpack/typescript-master/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HTMLPlugin({
+		title: 'My App 3',
+		filename: 'index.html',
+		template: 'src/assets/index.html'
+	})
+  ]
+};
