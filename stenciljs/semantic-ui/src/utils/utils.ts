3ccef223578f0333cdda44a9d1cb18e597ce4c8f
Edson Carli <gandbranco@gmail.com>
2019-03-09 07:47:59 -0300

    Stencil Semantic Ui Example

---
+++ stenciljs/semantic-ui/src/utils/utils.ts
@@ -0,0 +1,9 @@
+
+export function format(first: string, middle: string, last: string): string {
+  return (
+    (first || '') +
+    (middle ? ` ${middle}` : '') +
+    (last ? ` ${last}` : '')
+  );
+}
+ 
